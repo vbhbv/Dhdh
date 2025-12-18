d6b7b78fdd22e19e95eac034060f3b8c72ff9fbf
mjwl <72283979+vbhbv@users.noreply.github.com>
2025-12-18 03:29:09 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# إعدادات عامة للبوت
+TOKEN = "6807502954:AAH5tOwXCjRXtF65wQFEDSkYeFBYIgUjblg"
+SUMMARY_RATIO = 0.05  # نسبة التلخيص
+MAX_MESSAGE_LENGTH = 4000  # الحد الأقصى لرسالة تلغرام
+DOWNLOAD_FOLDER = "downloads/"
